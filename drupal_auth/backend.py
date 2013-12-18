b7844cf78d0b0f5a8489138732a15c4d40967c93
Jayaram Pai <rush2jrp@gmail.com>
2013-12-18 12:29:04 +0530

    updating wsgi.py

--- drupal_auth/backend.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import User
-
-from website.models import Test
-
-class DrupalAuthBackend:
-    def authenticate(self, username=None, password=None):
-        user = User.objects.get(username='admin')
-        return user
