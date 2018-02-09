0e04497dab21a3e801122fb35842b1576fd4c579
prashantsinalkar <prashantsinalkar@gmail.com>
2018-02-09 17:30:29 +0530

    aaded new configration

--- forums/local.py
+++ forums/local.py
@@ -1,7 +1,30 @@
-DB_USER='root'
-DB_PASS='root'
+DB_USER='dbuser'
+DB_PASS='dbpassowrd'
 PUB_KEY='6LemngMTAAAAAAC0Fkv0CQcavkTIIJ3LTDzi9gMq'
 PRIV_KEY='6LemngMTAAAAAHZsIaY-G98QegM3htdGVEWbEJYL'
-TO_EMAIL_ID = 'priyanka@fossee.in'
+TO_EMAIL_ID = 'to email id'
 SET_SITE_ID = 2
 
+#SENDER_EMAIL = "forums@fossee.in"
+#FORUM_NOTIFICATION = "forum-notifications@fossee.in"
+
+SENDER_EMAIL = "sender email id"
+FORUM_NOTIFICATION = "forum notification email id"
+
+EMAIL_URL = "email url"
+
+DOMAIN_NAME = "domain name" #http://abc@in
+# Host for sending e-mail.
+EMAIL_HOST_SERVER = 'email host server'
+
+# Port for sending e-mail.
+EMAIL_PORT_SERVER = "email port sever"
+
+# Optional SMTP authentication information for EMAIL_HOST.
+EMAIL_HOST_USER_SERVER = 'email host user'
+EMAIL_HOST_PASSWORD_SERVER = 'email host user password'
+EMAIL_USE_TLS_SERVER = 'Use TLS'
+
+
+FORUM_GOOGLE_RECAPTCHA_SECRET_KEY = 'google recaptcha secret key'
+FORUM_GOOGLE_RECAPTCHA_SITE_KEY = 'google recaptcha site key'
