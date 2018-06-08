1580117116f9ca4c41c2738fa67a41fc9cde6e2b
chaitanyabaranwal <chaitanyabaranwal@gmail.com>
2018-06-08 10:37:45 +0530

    Update spamFilter

--- website/cleanText.py
+++ website/cleanText.py
@@ -18,7 +18,7 @@ def cleanString(myString):
     myString = soup.get_text()
     numberLink = len(soup.find_all('a'))
     numberImg = len(soup.find_all('img'))
-    myString = myString + numberLink * " linktag " + numberImg * ' imgtag '
+    myString = myString + numberLink * " linktag " + numberImg * " imgtag "
 
     # convert numbers to 'number'
     myString = re.sub(r'[0-9]+', r' number ', myString)
@@ -26,23 +26,21 @@ def cleanString(myString):
     # convert $, ! and ? to proper words
     myString = re.sub(r'[$]', r' dollar ', myString)
     myString = re.sub(r'[!]', r' exclammark ', myString)
+    myString = re.sub(r'[?]', r' questmark ', myString)
 
     # convert other punctuation to whitespace
     myString = re.sub(r'([^\w\s]+)|([_-]+)', r' ', myString)
 
     # convert newlines and blanklines to special strings and extra whitespace to single
-    myString = re.sub(r'\n', r' ', myString)
-    myString = re.sub(r'\n\n', r' ', myString)
+    myString = re.sub(r'\n', r' newline ', myString)
+    myString = re.sub(r'\n\n', r' blankline ', myString)
     myString = re.sub(r'\s+', r' ', myString)
     myString = myString.strip(' ')
 
-    # remove all useless stopwords
-    myStringWords = myString.split(' ')
-    keepWords = [word for word in myStringWords if word not in nltk.corpus.stopwords.words('english')]
-
     # perform word stemming
+    myStringWords = myString.split(' ')
     stemmer = nltk.stem.snowball.SnowballStemmer('english')
-    stemWords = [stemmer.stem(word) for word in keepWords]
+    stemWords = [stemmer.stem(word) for word in myStringWords]
     myString = ' '.join(stemWords)
 
     return myString
--- website/spamFilter.py
+++ website/spamFilter.py
@@ -3,9 +3,9 @@
 import openpyxl
 import numpy as np
 from cleanText import cleanString
-from collections import Counter
 from sklearn.svm import LinearSVC
-from sklearn.metrics import confusion_matrix, f1_score
+from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 # Get the original dataset
 def store():
@@ -21,19 +21,12 @@ def store():
     for i in range(2, rows+1):
 
         if (str(dataSheetOld.cell(row = i, column = 2).value) != 'None'):
-            if (str(dataSheetOld.cell(row = i, column = 2).value) == '1'):
-                for k in range(4):
-                    yData.append(1)
-                    xData.append(cleanString(dataSheetOld.cell(row = i, column = 1).value.encode('utf-8')))
-
-    for i in range(2, rows+1):
-
-        if (str(dataSheetOld.cell(row = i, column = 2).value) != 'None'):
-            if (str(dataSheetOld.cell(row = i, column = 2).value) == '0'):
+            xData.append(str(cleanString(dataSheetOld.cell(row = i, column = 1).value)))
+            if (str(dataSheetOld.cell(row = i, column = 2).value) == "1"):
+                yData.append(1)
+            else:
                 yData.append(0)
-                xData.append(cleanString(dataSheetOld.cell(row = i, column = 1).value.encode('utf-8')))
-        
-    
+
     return xData, yData
 
     # # NOTE: to train data on the entire dataset, simply return xData and yData
@@ -41,87 +34,49 @@ def store():
     # xTrain, xTest, yTrain, yTest = train_test_split(xData, yData, test_size = 0.2, random_state = 42)
     # return xTrain, xTest, yTrain, yTest
 
-# make a dictionary of the most common words
-def makeDictionary(xData):
-
-    all_words = []
-    
-    for mail in xData:
-
-        words = mail.split()
-        all_words.extend(words)
-    
-    dictionary = Counter(all_words)
-    dictionary = dictionary.most_common(3000)
-    return dictionary
-
-# construct a feature vector for each mail
-def extractFeatures(xData, dictionary):
-    
-    featureMatrix = np.zeros((len(xData), 3000))
-    emailId = 0
-
-    for mail in xData:
-        for word in mail:
-            wordId = 0
-            for i,d in enumerate(dictionary):
-                if (d[0] == word):
-                    wordId = i
-                    featureMatrix[emailId, wordId] = mail.count(word)
-        emailId += 1
-
-    return featureMatrix
-
+# Train the data when server checks happening
 def train():
 
     print("Training spam filter...")
 
     # Create training data
     xTrain, yTrain = store()
+    global vectorizer
+    xTrainMatrix = vectorizer.fit_transform(xTrain)
+    yTrainMatrix = np.asarray(yTrain)
 
-    global trainDictionary
-    trainDictionary = makeDictionary(xTrain)
-
-    # Create feature vector and matrix for yTrain and xTrain
-    yTrainMatrix = np.zeros(len(yTrain))
-    for i in range(len(yTrain)):
-        if (yTrain[i] == 1):
-            yTrainMatrix[i] = 1
-
-    xTrainMatrix = extractFeatures(xTrain, trainDictionary)
-
-    # Training SVM classifier
     global model
-    model = model.fit(xTrainMatrix, yTrainMatrix)
+    model.fit(xTrainMatrix, yTrainMatrix)
+
 
 # Calculating the F-score
-def calcFScore(xTest, yTest):
+def calcFScore(xTest, yTest, model, vectorizer):
 
-    xTestMatrix = extractFeatures(xTest, trainDictionary)
-    yTestMatrix = np.zeros(len(yTest))
-    for i in range(len(yTest)):
-        if (yTest[i] == 1):
-            yTestMatrix[i] = 1
+    xTestMatrix = vectorizer.transform(xTest)
+    yTestMatrix = np.asarray(yTest)
 
     result = model.predict(xTestMatrix)
     matrix = confusion_matrix(yTestMatrix, result)
 
-    fScore = f1_score(yTestMatrix, result, pos_label = 0)
-    return fScore, matrix
+    fScore = f1_score(yTestMatrix, result, pos_label=0)
+    precision = precision_score(yTestMatrix, result, pos_label=0)
+    recall = recall_score(yTestMatrix, results, pos_label=0)
+    return fScore, precision, recall, matrix
 
 # Test new data for Spam
 def predict(emailBody):
 
-    # featureMatrix = extractFeatures([cleanString(emailBody)], trainDictionary)
-    # result = model.predict(featureMatrix)
-    # print("Predicting...")
+    global vectorizer
+    featureMatrix = vectorizer.transform([cleanString(emailBody)])
+    global model
+    result = model.predict(featureMatrix)
+    print("Predicting...")
 
-    # if (1 in result):
-    #     return "Spam"
-    # else:
-    #     return "Not Spam"
-    return "Not Spam"
+    if (1 in result):
+        return "Spam"
+    else:
+        return "Not Spam"
 
-# model = LinearSVC()
-# trainDictionary = {}
-# train()
+model = LinearSVC(class_weight='balanced')
+vectorizer = TfidfVectorizer(stop_words='english', max_df=75)
+train()
