b456a1a7f83459157b3832b0910affb99e66e633
prashantsinalkar <prashantsinalkar@gmail.com>
2019-11-22 19:26:43 +0530

    added dummy local file

---
+++ forums/local.py
@@ -0,0 +1,51 @@
+DB_USER = 'root'
+DB_PASS = 'root'
+DB_NAME = 'forums_djago2_1'
+PUB_KEY = 'Public Key'
+PRIV_KEY = 'Private Key'
+TO_EMAIL_ID = 'to@email.id'
+SET_SITE_ID = 2
+
+SITE_DEBUG = ['False'] #set debug option
+
+SENDER_EMAIL = 'sender@mail.com'
+BCC_EMAIL_ID = 'bcc@mail.com'
+FORUM_NOTIFICATION = 'forumnotifications@email.id'
+
+EMAIL_URL = 'Site URL for email content'
+
+DOMAIN_NAME = '#http://domain.name'
+# Host for sending e-mail.
+EMAIL_HOST_SERVER = 'localhost'
+
+# Port for sending e-mail.
+EMAIL_PORT_SERVER = 25
+
+# Optional SMTP authentication information for EMAIL_HOST.
+#EMAIL_HOST_USER_SERVER = 'email host user'
+#EMAIL_HOST_PASSWORD_SERVER = 'email host user password'
+#EMAIL_USE_TLS_SERVER = 'Use TLS'
+
+
+ADMINS_EMAIL_ADDRESS = '[Admins mail address]' #[('name', 'id@email.id'), ('name', 'id@email.id')]
+
+SERVER_EMAIL_ADDRESS = 'server email address'
+
+
+FORUM_GOOGLE_RECAPTCHA_SECRET_KEY = 'FORUM_GOOGLE_RECAPTCHA_SECRET_KEY'
+FORUM_GOOGLE_RECAPTCHA_SITE_KEY = 'FORUM_GOOGLE_RECAPTCHA_SITE_KEY'
+
+#Train spamfilter(default set as True)
+TRAIN_SPAMFILTER = True
+
+#ALLOWED_HOSTS
+SYSTEM_ALLOWED_HOSTS = ['Enter the host name' ]
+
+#Secre key for application (any random 32 alphanumeric characters)
+SECRET_KEY_CODE = 'Any random 32 alphanumeric charaters'
+
+
+
+# Maximum file size in MB
+MAX_FILE_SIZE_MB = 2
+
