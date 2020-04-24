3e309c8bc546415b9a36198cad6fc92e84e35c75
Priyansh Garg <priyanshgarg30@gmail.com>
2020-04-24 20:21:25 +0530

    Fix :: Ans edit opened textarea sometimes

--- website/templatetags/helpers.py
+++ website/templatetags/helpers.py
@@ -30,3 +30,10 @@ def get_recent_questions(questions):
 
 
 register.filter('get_recent_questions', get_recent_questions)
+
+
+# Concatenating two strings in template
+@register.filter
+def addstr(arg1, arg2):
+    """concatenate arg1 & arg2"""
+    return str(arg1) + str(arg2)
