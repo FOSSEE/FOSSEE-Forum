edd3f59be5ffa76a52d3b11fe5172a1c938e0a77
Priyansh Garg <priyanshgarg30@gmail.com>
2020-05-24 05:37:36 +0530

    reconfigured forums urls.py

--- forums/urls.py
+++ forums/urls.py
@@ -1,4 +1,4 @@
-from django.urls import include, path, re_path
+from django.urls import include, path
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.conf import settings
@@ -9,42 +9,11 @@ from forums import views
 from django.contrib.auth import views as auth_views
 
 admin.autodiscover()
+
 urlpatterns = [
 
-    re_path(r'^admin/', admin.site.urls),
-    re_path(r'^', include('website.urls', namespace='website')),
-
-    # URLs for password reset and password change
-    re_path('^forgotpassword/', auth_views.PasswordResetView.as_view(
-                template_name= 'forums/templates/registration/password_reset_form.html',
-                email_template_name = 'forums/templates/registration/password_reset_email.html',
-                subject_template_name = 'forums/templates/registration/password_reset_subject.txt',
-                from_email = settings.SENDER_EMAIL),
-            name = "password_reset",
-    ),
-
-    re_path(r'^password_reset/(?P<uidb64>[0-9A-Za-z]+)/(?P<token>.+)/$',\
-    auth_views.PasswordResetConfirmView.as_view(\
-    template_name= 'forums/templates/registration/password_reset_confirm.html'\
-    ), name = 'password_reset_confirm'),
-
-    re_path(r'^password_reset/mail_sent/', \
-    auth_views.PasswordResetDoneView.as_view(template_name= \
-    'forums/templates/registration/password_reset_done.html'),\
-    name = 'password_reset_done'),
-
-    re_path(r'^password_reset/complete/', auth_views.\
-    PasswordResetCompleteView.as_view(template_name= \
-    'forums/templates/registration/password_reset_complete.html'),\
-    name = 'password_reset_complete'),
-
-    re_path(r'^changepassword/', auth_views.PasswordChangeView.as_view(\
-    template_name='forums/templates/registration/password_change_form.html'), name = 'password_change'),
-
-    re_path(r'^password_change/done/', auth_views.\
-    PasswordChangeDoneView.as_view(template_name= \
-    'forums/templates/registration/password_change_done.html'),\
-    name = 'password_change_done'),
+    path('admin/', admin.site.urls),
+    path('', include('website.urls', namespace='website')),
 
     # User account URLs
     path('accounts/login/', views.user_login, name = 'user_login'),
@@ -55,6 +24,46 @@ urlpatterns = [
     path('accounts/view-profile/<int:user_id>/', views.account_view_profile, name = 'view_profile'),
 
     path('ckeditor/', include('ckeditor_uploader.urls')),
+
+    # Django Auth URLs
+    path('forgotpassword/',
+         auth_views.PasswordResetView.as_view(
+            template_name='forums/templates/registration/password_reset_form.html',
+            email_template_name ='forums/templates/registration/password_reset_email.html',
+            subject_template_name ='forums/templates/registration/password_reset_subject.txt',
+            from_email=settings.SENDER_EMAIL),
+         name="password_reset",
+    ),
+
+    path('password_reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+            template_name='forums/templates/registration/password_reset_confirm.html'), 
+         name='password_reset_confirm',
+    ),
+
+    path('password_reset/mail_sent/',
+         auth_views.PasswordResetDoneView.as_view(
+            template_name='forums/templates/registration/password_reset_done.html'),
+         name='password_reset_done',
+    ),
+
+    path('password_reset/complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+            template_name='forums/templates/registration/password_reset_complete.html'),
+         name='password_reset_complete',
+    ),
+
+    path('changepassword/',
+         auth_views.PasswordChangeView.as_view(
+            template_name='forums/templates/registration/password_change_form.html'),
+         name='password_change',
+    ),
+
+    path('password_change/done/',
+         auth_views.PasswordChangeDoneView.as_view(
+            template_name='forums/templates/registration/password_change_done.html'),
+         name='password_change_done',
+    ),
 
 ]
 
